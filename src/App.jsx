2bd50cc6accafecaa0cb7e3802153c011743fde0
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-09-11 10:13:40 -0500

    add basic css style and list organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+
   return (
     <main>
       <h1>To Do List</h1>
+      <div className="input-container">
+        <input id="input to-do" type="text" placeholder=''/>
+        <button id="add-todo">+</button>
+      </div>
+      <div className="container">
+        <div className="todo">
+          <p>Write Code</p>
+          <div className="actions">
+            <input type="checkbox" />
+            <button>Delete</button>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
