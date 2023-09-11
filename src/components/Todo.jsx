5936e72578d9a49cd0d347d0d6b07f5f72d8d880
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-09-11 17:43:10 -0500

    separate containers into components, separate components into individual files

---
+++ src/components/Todo.jsx
@@ -0,0 +1,13 @@
+const Todo = ({todo, index, deleteTodo}) => {
+    return (
+        <div className="todo">
+            <p>{todo}</p>
+            <div className="actions">
+            <input type="checkbox" />
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+            </div>
+        </div>
+    )
+}
+
+export default Todo
