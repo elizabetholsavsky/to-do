5f743920ac3e69b27a3d1c793c10fee5a1b2d174
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-09-11 10:30:37 -0500

    add state, add fn to write to do and check in console, add function to add to do to arr and check in console, map arr to dynamically create list iitems

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,39 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputVal, setInputVal] = useState("");
+
+  function writeTodo(e) {
+    setInputVal(e.target.value)
+    console.log(inputVal);
+  }
+
+  function addTodo() {
+    setTodos(prevTodos => [...prevTodos, inputVal]);
+    console.log(todos);
+  }
 
   return (
     <main>
       <h1>To Do List</h1>
       <div className="input-container">
-        <input id="input to-do" type="text" placeholder=''/>
-        <button id="add-todo">+</button>
+        <input type="text" placeholder="Enter To Do" onChange={writeTodo}/>
+        <button onClick={addTodo}>+</button>
       </div>
       <div className="container">
-        <div className="todo">
-          <p>Write Code</p>
-          <div className="actions">
-            <input type="checkbox" />
-            <button>Delete</button>
-          </div>
-        </div>
+        {todos.map((todo) => {
+            return (
+            <div className="todo">
+              <p>{todo}</p>
+              <div className="actions">
+                <input type="checkbox" />
+                <button>Delete</button>
+              </div>
+            </div>
+            );
+          })}
+        
       </div>
     </main>
   )
